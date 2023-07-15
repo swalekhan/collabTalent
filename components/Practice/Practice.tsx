6481f047d69ba6416968b6aea188d3d3ea5c2559
swalekhan <swalekhan1522@gmail.com>
2023-07-15 21:52:54 +0530

    emailcollacter

--- components/Practice/Practice.tsx
+++ components/Practice/Practice.tsx
@@ -4,17 +4,9 @@ import style from './Practice.module.css';
 import { practItems } from "@/utils/data";
 import { useState } from "react"
 import Image from "next/image";
+import EmailCollactor from "../EmailCollector/EmailCollactor";
 
 const Practice = () => {
-    const [email, setEmail] = useState(false)
-    const submitHandler = (e: any) => {
-        e.preventDefault()
-        setTimeout(() => {
-            e.target.email.value = ""
-        }, 1000)
-        setEmail(true)
-    }
-
     return (
         <section className={style.practice}>
             <div className={style.practice_img}>
@@ -24,12 +16,8 @@ const Practice = () => {
                 <h4>
                     We give data driven feedback so you can perform at their best
                 </h4>
-                <div className={style.practice_form} id="#">
-                    <form onSubmit={submitHandler} method="post">
-                        <input type="email" name="email" placeholder="Please enter email" required />
-                        <button type="submit">try it</button>
-                    </form>
-                    {email && <p className={style.success}>form submitted successfully</p>}
+                <div className={style.practice_email_collactor} >
+                <EmailCollactor/>
                 </div>
             </div>
         </section>
